ed1c26be8afc40efb810654ad6595d602465e6e4
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-03 22:30:33 +0900

    created Weather component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+
 function App() {
-  return <div className="App container">Weather App</div>;
+  return (
+    <div className="container">
+      <h1 className="App text-center mt-5">How's the weather today?</h1>
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Weather() {
+  return <div className="container Weather">This is the weather</div>;
+}
