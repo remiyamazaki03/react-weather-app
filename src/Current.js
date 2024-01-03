506bba78d9549ff23cbe5b1d39e8fc18902f13f5
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-03 23:20:18 +0900

    added high/low temp barebones to Current

--- src/Current.js
+++ src/Current.js
@@ -3,18 +3,20 @@ import React from "react";
 export default function Current() {
   return (
     <div className="container">
-      <div className="row ">
+      <div className="row mt-4">
         <div className="col-8 d-flex flex-column">
-          <div className="ps-2 labels" id="timeDisplay">
+          <div className="labels" id="timeDisplay">
             Oct 26 12:36
           </div>
-          <h1 className="ms-1" id="cityDisplay">
-            Enter a city
-          </h1>
+          <h1 id="cityDisplay">Enter a city</h1>
         </div>
         <div className="col-4 d-flex flex-column">
-          <div className="labels" id="windSpeed"></div>
-          <div className="labels d-none d-md-block" id="weatherDisplay"></div>
+          <div className="labels" id="windSpeed">
+            Wind
+          </div>
+          <div className="labels d-none d-md-block" id="weatherDisplay">
+            Sunny
+          </div>
           <img
             src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
             alt="weather icon"
@@ -22,6 +24,27 @@ export default function Current() {
             id="iconToday"
           />
         </div>
+      </div>
+      <div class="row">
+        <h5>
+          <span id="average">0</span>
+          <a href="#" id="toCelsius">
+            °C
+          </a>
+          |
+          <a href="#" id="toFahrenheit">
+            °F
+          </a>
+          (
+          <span class="high" id="highToday">
+            high°C
+          </span>
+          |
+          <span class="low" id="lowToday">
+            low°C
+          </span>
+          )
+        </h5>
       </div>
     </div>
   );
